1d7442db5e335561ae0445e39d20c1ff94a3e8b4
Milen Todev <78319110+todevmilen@users.noreply.github.com>
2022-08-23 21:36:09 +0300

    design: styling sidebar comp

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -6,6 +6,7 @@ const Sidebar = () => {
             <div className="top">
                 <span className='logo'>todev-admin</span>
             </div>
+            <hr />
             <div className="center">
                 <ul>
                     <li>
