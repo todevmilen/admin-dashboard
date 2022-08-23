771c4afcd39244bd54804e2448c764382a4032af
Milen Todev <78319110+todevmilen@users.noreply.github.com>
2022-08-23 21:02:23 +0300

    feat: set SCSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Home from "./pages/home/Home";
 function App() {
   return (
     <div className="App">
-      Todev
+      <Home />
     </div>
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './home.scss';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='home'>Home</div>
     )
 }
 
