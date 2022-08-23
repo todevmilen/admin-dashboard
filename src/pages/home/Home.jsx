f1208d24321071c9b40d4f85947bbde242353464
Milen Todev <78319110+todevmilen@users.noreply.github.com>
2022-08-23 21:05:26 +0300

    feat: List Component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,9 @@ import './home.scss';
 
 const Home = () => {
     return (
-        <div className='home'>Home</div>
+        <div className='home'>
+            home
+        </div>
     )
 }
 
---
+++ src/pages/list/List.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '.list.scss'
+
+const List = () => {
+    return (
+        <div className="list">List</div>
+    )
+}
+
+export default List
