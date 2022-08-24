a1233bba82563e94289045a975c7fb55708d6d76
Milen Todev <78319110+todevmilen@users.noreply.github.com>
2022-08-24 21:25:57 +0300

    feat: widget component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,7 +11,10 @@ const Home = () => {
             <div className="homeContainer">
                 <Navbar />
                 <div className="widgets">
-                    <Widget />
+                    <Widget type="user" />
+                    <Widget type="order" />
+                    <Widget type="earning" />
+                    <Widget type="balance" />
                 </div>
                 home container
             </div>
